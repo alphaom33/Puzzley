17aa3105634596e7d456f3bd10d4a7bddf2a25cb
Dinobird11 <ttrex997@outlook.com>
2025-07-31 08:26:42 -0500

    jump body

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -139,7 +139,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Body"))
         {
             onGround = true;
             jumpBuffering = false;

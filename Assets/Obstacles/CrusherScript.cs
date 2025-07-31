963832f2d8d85529a20aeb0de6d87dcfe14ae130
Dinobird11 <ttrex997@outlook.com>
2025-07-31 14:24:36 -0500

    crusher and sprites

--- Assets/Obstacles/CrusherScript.cs
+++ Assets/Obstacles/CrusherScript.cs
@@ -7,8 +7,8 @@ public class CrusherScript : MonoBehaviour
 {
 
 
-   public Rigidbody2D Rigidbody2D;
-   
+    public Rigidbody2D Rigidbody2D;
+
     public float CrusherUpAmount;
     public float CrusherInterval;
     public float CrusherTimeDown;
@@ -19,39 +19,39 @@ public class CrusherScript : MonoBehaviour
         StartCoroutine(Crush());
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Body") || collision.gameObject.CompareTag("Ground"))
+        {
+            StartCoroutine(Rise());
+        }
+        else if (collision.gameObject.CompareTag("Player") && collision.transform.position.y < transform.position.y)
         {
             GameManager.GetInstance().KillPlayer();
         }
-        else if (collision.gameObject.CompareTag("Body") || collision.gameObject.CompareTag("Ground"))
+        
+    }
+
+        private IEnumerator Crush()
         {
-            StartCoroutine(Rise());
 
+            yield return new WaitForSeconds(CrusherInterval); // time it stays up before coming down to crush
+
+
+            Rigidbody2D.bodyType = RigidbodyType2D.Dynamic; // allows the crusher to fall down
         }
-    }
+        private IEnumerator Rise()
+        {
+            yield return new WaitForSeconds(CrusherTimeDown); // keeps the crush down for as long as you want
 
-    private IEnumerator Crush()
-    {
+            transform.DOMoveY(CrusherUpAmount, 1); // raises the crusher/
 
-        yield return new WaitForSeconds(CrusherInterval); // time it stays up before coming down to crush
-        
-        
-        Rigidbody2D.bodyType = RigidbodyType2D.Dynamic; // allows the crusher to fall down
-    }
-    private IEnumerator Rise()
-    {
-        yield return new WaitForSeconds (CrusherTimeDown); // keeps the crush down for as long as you want
-       
-        transform.DOMoveY(CrusherUpAmount,1); // raises the crusher/
+            Rigidbody2D.bodyType = RigidbodyType2D.Static;// makes it so the crusher stays in the air, and then counts down tell it crushes again
+            StartCoroutine(Crush()); // allows crusher to come down, after the interval
 
-        Rigidbody2D.bodyType = RigidbodyType2D.Static;// makes it so the crusher stays in the air, and then counts down tell it crushes again
-        StartCoroutine(Crush()); // allows crusher to come down, after the interval
+            yield return new WaitForSeconds(CrusherInterval);
+            Rigidbody2D.bodyType = RigidbodyType2D.Dynamic;
+        }
 
-        yield return new WaitForSeconds(CrusherInterval);
-        Rigidbody2D.bodyType = RigidbodyType2D.Dynamic;
-    }
 
-   
-}
+    } 
